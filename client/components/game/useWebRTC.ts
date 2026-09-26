31cf80c8e37e3a82a6f0014ac7345ad1e69ad74f
Bedir Hüseyin Özcan <bedir.example@gmail.com>
2026-09-26 15:33:11 +0300

    feat(webrtc): add TURN server configurations for improved connectivity

--- client/components/game/useWebRTC.ts
+++ client/components/game/useWebRTC.ts
@@ -1,4 +1,4 @@
-﻿import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Socket } from 'socket.io-client';
 import { User } from './types';
 import { toast } from 'react-toastify';
@@ -44,7 +44,22 @@ export function useWebRTC(socket: Socket | null, myId: string, isVoiceEnabled: b
                     { urls: 'stun:stun1.l.google.com:19302' },
                     { urls: 'stun:stun2.l.google.com:19302' },
                     { urls: 'stun:stun3.l.google.com:19302' },
-                    { urls: 'stun:stun4.l.google.com:19302' }
+                    { urls: 'stun:stun4.l.google.com:19302' },
+                    {
+                        urls: "turn:openrelay.metered.ca:80",
+                        username: "openrelayproject",
+                        credential: "openrelayproject"
+                    },
+                    {
+                        urls: "turn:openrelay.metered.ca:443",
+                        username: "openrelayproject",
+                        credential: "openrelayproject"
+                    },
+                    {
+                        urls: "turn:openrelay.metered.ca:443?transport=tcp",
+                        username: "openrelayproject",
+                        credential: "openrelayproject"
+                    }
                 ]
             });
 
